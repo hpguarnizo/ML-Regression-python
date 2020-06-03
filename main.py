72e84114f67394c0146865deb07b33b8dcd979f2
Alberto Perdomo <alperdomo1988@gmail.com>
2020-06-03 07:27:04 -0500

    v.101

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from utils import Utils
 from model import Models
-
+import pandas as pd
 
 if __name__=='__main__':
     utils = Utils()
@@ -9,7 +9,6 @@ if __name__=='__main__':
     train = utils.load_data('./csv/clean_train.csv')
     test = utils.load_data('./csv/clean_test.csv')
     test.drop('SalePrice', axis=1, inplace=True)
-    print(test.isnull().sum())
     #X, y = utils.features_target(train, ['SalePrice'], ['SalePrice'])
     #best_model, best_score = models.grid_training(X,y)
     #print(best_model)
